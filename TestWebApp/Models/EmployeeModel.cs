4c17a9aed8c97617cdf3d2a2348c057fb56d2f4b
Prabhuling Biradar <prabhuling66@gmail.com>
2021-01-13 12:47:41 +0530

    Adding employee model

---
+++ TestWebApp/Models/EmployeeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebApp.Models
+{
+    public class EmployeeModel
+    {
+        public int Empid { get; set; }
+        public string Name { get; set; }
+    }
+}
